76ed12df0cb229e173b4467357fc7d8e0bd27c4c
jaskieerr <nizar.zairat@gmail.com>
2024-01-14 05:17:34 -0800

    fingers crossed

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 '''File storage thingy'''
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
     '''JSON serialization and deserialization'''
-    __file_path = "file.json"
-    __objects = {}
+    __file_path: str = "file.json"
+    __objects: dict = dict()
 
     def all(self):
         '''Returns the dictionary __objects.'''
@@ -19,27 +26,42 @@ class FileStorage:
 
     def save(self):
         '''Serializes __objects to the JSON file (path: __file_path).'''
-        data = {}
-        for key, obj in self.__objects.items():
-            data[key] = obj.to_dict()
+        temvar1: dict = {k: v.to_dict() for k, v in self.__objects.items()}
 
-        try:
-            with open(self.__file_path, 'w', encoding='utf-8') as file:
-                json.dump(data, file)
-        except Exception as e:
-            print(f"Error saving file: {e}")
+        with open(self.__file_path, 'w', encoding='utf-8') as f:
+            json.dump(temvar1, f)
 
-    def reload(self):
+    def destroy(self, key):
         '''Deserializes the JSON file to __objects.'''
         try:
-            with open(self.__file_path, 'r', encoding='utf-8') as file:
-                data = json.load(file)
+            del self.__objects[key]
 
-            for key, obj_dict in data.items():
-                class_name, obj_id = key.split('.')
-                cls = globals()[class_name]
-                obj = cls(**obj_dict)
-                self.__objects[key] = obj
+            self.save()
+            return True
+        except KeyError:
+            return False
 
-        except FileNotFoundError:
+    def reload(self):
+        '''Deserializes the JSON file to __objects
+        (only if the JSON file (__file_path) exists
+        otherwise, do nothing. If the file doesn’t
+        exist, no exception should be raised).
+        '''
+
+        __model_classes = {"BaseModel": BaseModel,
+                           "User": User,
+                           "Place": Place,
+                           "State": State,
+                           "City": City,
+                           "Amenity": Amenity,
+                           "Review": Review}
+        tempvar2: dict = {}
+        try:
+            with open(self.__file_path, 'r', encoding='utf-8') as f:
+                tempvar2 = json.loads(f.read())
+            for key, value in tempvar2.items():
+                sckey = key.split(".")[0]
+                if sckey in __model_classes.keys():
+                    self.__objects[key] = __model_classes[sckey](**value)
+        except (KeyError, FileNotFoundError):
             pass
